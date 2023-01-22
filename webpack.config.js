aa7b9ab0f875d637742c465469e7cc21dc53cdd3
Ian Klug <ianklug123@gmail.com>
2023-01-22 10:21:05 -0800

    all of it

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: "production",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
