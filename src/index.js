2d69c4266bb3d897d97c40d8cb9ae0f96ef5dd65
Ian Klug <ianklug123@gmail.com>
2023-01-22 10:20:37 -0800

    beep boop

--- src/index.js
+++ src/index.js
@@ -1,58 +1,85 @@
-import "./styles.css";
+// import "./styles.css";
 
+const createDiv = (divClass, textContent) => {
+    const div = document.createElement('div');
+    div.classList.add(divClass);
+    div.textContent = textContent;
+    return div;
+}
+
+const childFunction = {
+    appendChildren: (daddy, obj) => {
+        for (let child in obj){
+             daddy.appendChild(obj[child]);
+        }
+    },
+    deleteAllChildren: (daddy) => {
+        while (daddy.firstChild){
+            daddy.removeChild(daddy.lastChild);
+        }
+    },
+   
+}
+
+const parent = {
+    originalDiv: document.querySelector('.content'),
+    tabContent: document.querySelector('.tab-content'),
+}
+
+const childrenObj = {
+    main: {
+        logo: createDiv('top-logo', 'The Risteronto'),
+    tabOrigin: createDiv('tabs', ''),
+    tabContent: createDiv('tab-content', ''),
+    },
+    tab: {
+        tab1: createDiv('tab', 'Home'),
+        tab2: createDiv('tab', 'Menu'),
+        tab3: createDiv('tab', 'About'),
+        tab4: createDiv('tab', 'Contact Us'),
+    },
+    tabContent: {
+        'Home': createDiv('main-drag', "Welcome home."),
+        'Menu': createDiv('main-drag', "Welcome menu."),
+        'About': createDiv('main-drag', "Welcome about."),
+        'Contact Us': createDiv('main-drag', "Don't ever speak to me."),
+    },
+}
 
 const htmlObj = {
 
-default: () => {
-    const originalDiv = document.querySelector('.content');
+    default: () => {
+        const originalDiv = parent.originalDiv;
+  
+        childrenObj.main.tabContent.appendChild(childrenObj.tabContent['Home']);
+        childFunction.appendChildren(childrenObj.main.tabOrigin, childrenObj.tab);
+        childFunction.appendChildren(originalDiv, childrenObj.main);
 
-    //function that creates div, gives it a class
-    //and assigns textContent
-
-    const createDiv = (divClass, textContent) => {
-        const div = document.createElement('div');
-        div.classList.add(divClass);
-        div.textContent = textContent;
-        return div;
-    }
-
-    const appendChildren = (daddy, obj) => {
-        for (let child in obj){
-console.log(child);
-console.log(obj.child);
-            console.log(obj[child]);
-           daddy.appendChild(obj[child]);
-        }
-    }
-    
-const children = {
-    logo: createDiv('top-logo', 'The Risteronto'),
-    tabOrigin: createDiv('tabs', ''),
-    tabContent: createDiv('tab-content', ''),
-}
-const tabChildren = {
-    tab1: createDiv('tab', 'Home'),
-    tab2: createDiv('tab', 'Menu'),
-    tab3: createDiv('tab', 'About'),
-    tab4: createDiv('tab', 'Contact Us'),
-}
-
-const tabContentChild = {
-    tab1: createDiv('main-drag', "Welcome home."),
-}
-
-appendChildren(children['tabContent'], tabContentChild);
-appendChildren(children['tabOrigin'], tabChildren);
-appendChildren(originalDiv, children);
-
-
-},
-
-
-
-
-
+    },
 };
 
 htmlObj.default();
-console.log('hello');
+
+document.querySelector('.content').addEventListener('click', (e)=>{
+    console.log(e.target);
+    const newTab = document.querySelector('.tab-content');
+    if(e.target.classList.contains('tab')){
+        console.log('clicked!');
+        e.target.classList.toggle('active');
+        let tabs = document.querySelector('.tabs').firstChild
+        while (tabs){
+            if(tabs!==e.target && tabs.classList.contains('active')){
+                tabs.classList.toggle('active');
+            }
+            tabs = tabs.nextSibling;
+        }
+
+        const clickedText = e.target.textContent;
+        childFunction.deleteAllChildren(newTab);
+
+        newTab.appendChild(childrenObj.tabContent[`${clickedText}`]);
+        
+  
+    }
+   
+});
